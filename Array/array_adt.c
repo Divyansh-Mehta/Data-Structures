9ae147bb53d7b05d27e258a003f54b2182f88057
Divyansh-Mehta <divyanshmehta01@gmail.com>
2021-06-01 22:18:27 +0530

    added more functions

--- Array/array_adt.c
+++ Array/array_adt.c
@@ -11,30 +11,37 @@ typedef struct Array_ADT
 } array;
 
 // Function Declaration
-void display(array);            // Display
-void append(array *, int);      // Append ie add at last
-int delete (array *, int);      // Deletion
-int l_search(array, int);       // Linear search
-int b_search(array, int);       // Binary search
-int get(array, int);            // Get element at particular index
-void set(array *, int, int);    // Replace element at particular index
-int max(array);                 // Return max element
-int min(array);                 // Return min element
-int sum(array);                 // Return sum of all elements of array
-int avg(array);                 // Return average of all elements of array
-void reverse_v1(array *);       // Reverse array using another array
-void reverse_v2(array *);       // Reverse array with using another array
-void swap(int *, int *);        // Function to swap
-void left_shift(array *);       // Shifts elements left and terminates 1st element
-void left_rotate(array *);      // shifts elements left and 1st element to last
-void right_shift(array *);      // Shifts elements right and terminates last element
-void right_rotate(array *);     // shifts elements left and last element to 1st
-void insert_sort(array *, int); // insert in a shorted array
-int check_sort(array);          // cheak if array is sorted
-void negative_on_left(array *); // put all negative element on left side of array
+void display(array);                            // Display
+void append(array *, int);                      // Append ie add at last
+int delete (array *, int);                      // Deletion
+int l_search(array, int);                       // Linear search
+int b_search(array, int);                       // Binary search
+int get(array, int);                            // Get element at particular index
+void set(array *, int, int);                    // Replace element at particular index
+int max(array);                                 // Return max element
+int min(array);                                 // Return min element
+int sum(array);                                 // Return sum of all elements of array
+int avg(array);                                 // Return average of all elements of array
+void reverse_v1(array *);                       // Reverse array using another array
+void reverse_v2(array *);                       // Reverse array with using another array
+void swap(int *, int *);                        // Function to swap
+void left_shift(array *);                       // Shifts elements left and terminates 1st element
+void left_rotate(array *);                      // Shifts elements left and 1st element to last
+void right_shift(array *);                      // Shifts elements right and terminates last element
+void right_rotate(array *);                     // Shifts elements left and last element to 1st
+void insert_sort(array *, int);                 // Insert in a shorted array
+int check_sort(array);                          // Cheak if array is sorted
+void negative_on_left(array *);                 // Put all negative element on left side of array
+array *merge(array *, array *);                 // Merge sorted array
+array *union_unsort(array *, array *);          // Union of unsorted arrays
+array *union_sorted(array *, array *);          // Union of sorted arrays
+array *intersection_unsorted(array *, array *); // Intersection of unsorted arrays
+array *intersection_sorted(array *, array *);   // Intersection of sorted array
+array *difference_unsorted(array *, array *);   // Difference of unsorted array first param - second param
+array *difference_sorted(array *, array *);     // Difference of sorted array first param - second param
 
 // main
-int main()
+int main() // Add in main merge, union_sorted, union_unsorted, intersection_sorted and unsorted, difference sorted and unsorted
 {
     array arr;
     int ch;
@@ -42,7 +49,7 @@ int main()
     {
         printf("\n*****MENU*****");
         printf("\n1.Create\n2.Display\n3.append\n4.delete\n5.search\n6.Get and Set\n7.Some common operation");
-        printf("\n8.Reverse\n9.Shift and Rotate\n10.Misc\n11.Exit\nEnter your choice: ");
+        printf("\n8.Reverse\n9.Shift and Rotate\n10.Misc\n11.Operation on two array\n12.Exit\nEnter your choice: ");
         scanf("%d", &ch);
 
         switch (ch)
@@ -174,12 +181,69 @@ int main()
             }
         }
         case 11:
+        {
+            int i, n;
+            array arr2, *arr3;
+            printf("\nEnter length another array: ");
+            scanf("%d", &arr2.length);
+            printf("Enter array elements: ");
+            for (i = 0; i < arr2.length; i++)
+            {
+                scanf("%d",&arr2.A[i]);
+            }
+            arr2.size = MAX;
+            printf("\n1.Merge\n2.Union\n3.Intersection\n4.Difference\nEnter your choice: ");
+            scanf("%d", &n);
+            switch (n)
+            {
+            case 1:
+            {
+                arr3 = merge(&arr, &arr2);
+                printf("\nMerger array: ");
+                for (i = 0; i < arr3->length; i++)
+                    printf("%d ", arr3->A[i]);
+            }
+            break;
+            case 2:
+            {
+                int i;
+                arr3 = union_unsort(&arr, &arr2);
+                printf("\nResultent array: ");
+                for (i = 0; i < arr3->length; i++)
+                    printf("%d ", arr3->A[i]);
+            }
+            break;
+            case 3:
+            {
+                int i;
+                arr3 = intersection_unsorted(&arr, &arr2);
+                printf("\nResultant array: ");
+                for (i = 0; i < arr3->length; i++)
+                    printf("%d ", arr3->A[i]);
+            }
+            break;
+            case 4:
+            {
+                int i;
+                arr3 = difference_unsorted(&arr, &arr2);
+                printf("\nMerger array: ");
+                for (i = 0; i < arr3->length; i++)
+                    printf("%d ", arr3->A[i]);
+            }
+            break;
+            default:
+                printf("\nWrong choice");
+            }
+        }
+        break;
+
+        case 12:
             break;
         default:
             printf("Enter correct choice");
         }
 
-    } while (ch != 11);
+    } while (ch != 12);
 
     return 0;
 }
@@ -427,4 +491,181 @@ void negative_on_left(array *arr)
         if (i < j)
             swap(&arr->A[i], &arr->A[j]);
     }
+}
+
+array *merge(array *arr1, array *arr2)
+{
+    array *arr3 = (array *)malloc(sizeof(array));
+    int i, j, k;
+    i = j = k = 0;
+    while (i < arr1->length && j < arr2->length)
+    {
+        if (arr1->A[i] < arr2->A[j])
+            arr3->A[k++] = arr1->A[i++];
+
+        else
+            arr3->A[k++] = arr2->A[j++];
+    }
+
+    for (; i < arr1->length; i++)
+        arr3->A[k++] = arr1->A[i];
+
+    for (; j < arr2->length; j++)
+        arr3->A[k++] = arr2->A[j];
+
+    arr3->length = arr1->length + arr2->length;
+    arr3->size = MAX;
+
+    return arr3;
+}
+
+array *union_unsort(array *arr1, array *arr2)
+{
+    array *arr3 = (array *)malloc(sizeof(array));
+    int i, j, k;
+    i = j = k = 0;
+    for (i = 0; i < arr1->length; i++)
+    {
+        arr3->A[k++] = arr1->A[i];
+    }
+    for (i = 0; i < arr2->length; i++)
+    {
+        for (j = 0; j < arr1->length; j++)
+        {
+            if (arr2->A[i] == arr1->A[j])
+            {
+                break;
+            }
+        }
+        if (j == arr1->length)
+            arr3->A[k++] = arr2->A[i];
+    }
+    arr3->size = MAX;
+    arr3->length = k;
+    return arr3;
+}
+
+array *union_sorted(array *arr1, array *arr2)
+{
+    array *arr3 = (array *)malloc(sizeof(array));
+    int i, j, k;
+    i = j = k = 0;
+    while (i < arr1->length && j < arr2->length)
+    {
+        if (arr1->A[i] < arr2->A[j])
+            arr3->A[k++] = arr1->A[i++];
+
+        else if (arr1->A[i] > arr2->A[j])
+            arr3->A[k++] = arr2->A[j++];
+        else
+        {
+            arr3->A[k++] = arr2->A[i++];
+            j++;
+        }
+    }
+
+    for (; i < arr1->length; i++)
+        arr3->A[k++] = arr1->A[i];
+
+    for (; j < arr2->length; j++)
+        arr3->A[k++] = arr2->A[j];
+
+    arr3->length = k;
+    arr3->size = MAX;
+
+    return arr3;
+}
+
+array *intersection_unsorted(array *arr1, array *arr2)
+{
+    array *arr3 = (array *)malloc(sizeof(array));
+    int i, j, k;
+    i = j = k = 0;
+    for (i = 0; i < arr1->length; i++)
+    {
+        for (j = 0; j < arr2->length; j++)
+        {
+            if (arr2->A[j] == arr1->A[i])
+            {
+                break;
+            }
+        }
+        if (j != arr2->length)
+            arr3->A[k++] = arr1->A[i];
+    }
+    arr3->size = MAX;
+    arr3->length = k;
+    return arr3;
+}
+
+array *intersection_sorted(array *arr1, array *arr2)
+{
+    array *arr3 = (array *)malloc(sizeof(array));
+    int i, j, k;
+    i = j = k = 0;
+    while (i < arr1->length && j < arr2->length)
+    {
+        if (arr1->A[i] < arr2->A[j])
+            i++;
+
+        else if (arr1->A[i] > arr2->A[j])
+            j++;
+        else
+        {
+            arr3->A[k++] = arr2->A[i++];
+            j++;
+        }
+    }
+
+    arr3->length = k;
+    arr3->size = MAX;
+
+    return arr3;
+}
+
+array *difference_unsorted(array *arr1, array *arr2)
+{
+    array *arr3 = (array *)malloc(sizeof(array));
+    int i, j, k;
+    i = j = k = 0;
+    for (i = 0; i < arr2->length; i++)
+    {
+        for (j = 0; j < arr1->length; j++)
+        {
+            if (arr2->A[j] == arr1->A[i])
+            {
+                break;
+            }
+        }
+        if (j == arr1->length)
+            arr3->A[k++] = arr1->A[i];
+    }
+    arr3->size = MAX;
+    arr3->length = k;
+    return arr3;
+}
+
+array *difference_sorted(array *arr1, array *arr2)
+{
+    array *arr3 = (array *)malloc(sizeof(array));
+    int i, j, k;
+    i = j = k = 0;
+    while (i < arr1->length && j < arr2->length)
+    {
+        if (arr1->A[i] < arr2->A[j])
+            arr3->A[k++] = arr1->A[i++];
+
+        else if (arr1->A[i] > arr2->A[j])
+            j++;
+        else
+        {
+            i++;
+            j++;
+        }
+    }
+
+    arr3->length = k;
+    arr3->size = MAX;
+
+    return arr3;
 }
